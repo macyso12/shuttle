166cc285a484261e4e72cbd399e281e1e5663666
macyso12 <macyso12@gmail.com>
2022-06-09 16:03:02 -0400

    added hover underline animation on NavBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Navbar />
     </Router>
-
   );
 }
 
--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,88 +1,84 @@
-import styled from 'styled-components'
-import { NavLink as Link } from 'react-router-dom'
-import { FaBars } from 'react-icons/fa'
-
+import styled from 'styled-components';
+import {NavLink as Link} from 'react-router-dom';
+import {FaBars} from 'react-icons/fa';
 
 export const Nav = styled.nav`
-    background: #ba181b;
-    height: 80px;
-    display: flex;
-    justify-content: space-between;
-    padding: 0.5rem 1rem;
-    z-index: 10;
-`
+  background: #ba181b;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem 1rem;
+  z-index: 10;
+`;
 
 export const NavLink = styled(Link)`
-    color: #fff;
-    font-weight: 500;
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    padding: 0 1rem;
-    height: 100%;
-    cursor: pointer;
+  color: #fff;
+  font-weight: 500;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  margin: 1rem;
+  margin-bottom: 0.5px
+  height: 100%;
+  cursor: pointer;
 
-    &:hover{
-        color: #6a040f;
-    }
-`
+  &:hover {
+    color: #6a040f;
+    border-bottom: 2px solid #6a040f;
+  }
+`;
 
 export const Bars = styled(FaBars)`
-    display: none;
-    color: white;
-    
-    @media screen and (max-width: 768px){
-        display: block;
-        position: absolute;
-        top: 0;
-        right: 0;
-        transform: translate(-100%, 75%);
-        font-size: 1.8rem;
-        cursor: pointer;
-    }
+  display: none;
+  color: white;
 
-`
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`;
 
 export const NavMenu = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 
-    @media screen and (max-width: 768px){
-        display: none;
-    }
-`
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
 
 export const NavBtn = styled.nav`
-    display: flex;
-    align-items: center;
-    margin-right: 24px
-
-    @media screen and (max-width: 768px){
-        display: none;
-    }
-
-`
+  display: flex;
+  align-items: center;
+  margin-right: 24px @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
 
 export const NavBtnLink = styled(Link)`
-    border-radius: 10px;
-    background: white;
-    padding: 10px 22px;
-    color: #a4161a;
-    border: none;
-    font-weight: 700;
-    outline: none;
-    cursor: pointer;
+  border-radius: 10px;
+  background: white;
+  padding: 10px 22px;
+  color: #a4161a;
+  border: none;
+  font-weight: 700;
+  outline: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+
+  &:hover {
     transition: all 0.2s ease-in-out;
-    text-decoration: none;
-    
-    &:hover{
-        transition: all 0.2s ease-in-out;
-        background: #df8080;
-        color: white;
-    }
+    background: #df8080;
+    color: black;
+  }
 
-    @media screen and (max-width: 768px){
-        display: none;
-    }
-
-`
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
