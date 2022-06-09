a35ae7d97a2118330db824f7b5680657c3ef08de
Truc Duong <91285203+trucdg@users.noreply.github.com>
2022-06-09 15:40:13 -0400

    Truc navbar (#1)
    * add components dir
    
    * add navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from './NavbarElements';
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <Nav>
+                <NavLink to="/">
+                    <h1>Shuttle</h1>
+                </NavLink>
+
+                <Bars />
+
+                <NavMenu>
+
+                    <input type="text" placeholder="Search..." />
+
+
+                    <NavLink to="/home" activeStyle>
+                        Home
+                    </NavLink>
+                    <NavLink to="/clubs" activeStyle>
+                        Clubs
+                    </NavLink>
+                    <NavLink to="/account" activeStyle>
+                        Account
+                    </NavLink>
+
+                    <NavBtn>
+                        <NavBtnLink to='/signin'>
+                            Sign In
+                        </NavBtnLink>
+                    </NavBtn>
+
+                </NavMenu>
+
+
+
+            </Nav>
+        </div>
+    );
+}
+
+export default Navbar
